d98a9a39d274271032ee03930e46059448fb2a4e
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-04-19 21:18:22 -0500

    first commit

---
+++ src/main/java/com/codigo/condoricoaquira/CondoriCoaquiraApplication.java
@@ -0,0 +1,13 @@
+package com.codigo.condoricoaquira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondoriCoaquiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondoriCoaquiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codigo/condoricoaquira/CondoriCoaquiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.condoricoaquira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondoriCoaquiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
